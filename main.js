7239611a6d4c4419b3350a7dfcb8ce024202f6d6
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2022-08-10 18:14:13 +0500

    completed social media app

---
+++ main.js
@@ -0,0 +1,54 @@
+var form = document.getElementById("form");
+var input = document.getElementById("input");
+var msg = document.getElementById("msg");
+var posts = document.getElementById("posts");
+
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    console.log("button clicked");
+
+    formValidation()
+})
+
+let formValidation = () => {
+    if (input.value === ""){
+        msg.innerHTML = "Post cannot be blank";
+        console.log("failure")
+    }
+    else{
+        console.log("success");
+        msg.innerHTML = "";
+        acceptData();
+    }
+}
+
+let data = {};
+
+let acceptData = () => {
+    data["text"] = input.value;
+    console.log(data);
+    createPost()
+}
+
+let createPost = () => {
+    posts.innerHTML += `
+    <div>
+        <p>${data.text}</p>
+        <span class="options">
+            <i onclick="editPost(this)" class="fas fa-edit"></i>
+            <i onclick="deletePost(this)" class="fas fa-trash-alt"></i>
+        </span>
+    </div>
+    `;
+    input.value="";
+}
+
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove(); 
+}
+let editPost = (e) => {
+    input.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+}
+
