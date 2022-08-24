b835fa5c5909845e1b41fb5e27af28f6e5a40097
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2022-08-24 12:14:05 +0500

    pushing the code to github

--- main.js
+++ main.js
@@ -47,8 +47,60 @@ let createPost = () => {
 let deletePost = (e) => {
     e.parentElement.parentElement.remove(); 
 }
+
 let editPost = (e) => {
     input.value = e.parentElement.previousElementSibling.innerHTML;
     e.parentElement.parentElement.remove();
 }
+
+// var form = document.getElementById("form")
+// var input = document.getElementById("input")
+// var msg = document.getElementById("msg")
+// var posts = document.getElementById("posts")
+
+// form.addEventListener("submit", (e)=> {
+//     e.preventDefault();
+//     console.log("Button Clicked");
+
+//     formValidation();
+// })
+
+// let formValidation = () => {
+//     if(input.value === ""){
+//         msg.innerHTML = "Post cannot be blank"
+//         console.log("failure")
+//     }
+//     else{
+//         console.log("success")
+//         msg.innerHTML = "";
+//         acceptData();
+//     }
+// }
+
+// let data = {};
+
+// let acceptData = () =>{
+//     data["text"] = input.value;
+//     console.log(data)
+//     createPost();
+// }
+
+// let createPost = () => {
+//     posts.innerHTML += `
+//     <div>
+//         <p>${data.text}</p>
+//         <span class="options">
+//             <i class="fas fa-edit"></i>
+//             <i class="fas fa-trash-alt"></i>
+//         </span>
+//     </div>
+//     `;
+//     input.value = "";
+// }
+
+
+
+
+
+
 
